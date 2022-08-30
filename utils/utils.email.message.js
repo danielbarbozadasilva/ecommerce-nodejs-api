638f2e28cf5d7ff1932239341bbbd24c9d489667
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-08-30 17:43:15 -0300

    chore: add sendgrid config

---
+++ utils/utils.email.message.js
@@ -0,0 +1,22 @@
+const Email = (title) => {
+  return ` 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+		<meta name="x-apple-disable-message-reformatting" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta content="telephone=no" name="format-detection" />
+		<title>${title}</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet" />
+	</head>
+	<body>
+		
+	</body>
+</html>
+
+    `;
+};
+
+module.exports = { Email };
