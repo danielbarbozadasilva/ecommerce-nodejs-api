ee6196077d65d7b32a0bcc67ad5c3903378536bf
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-02 21:56:06 -0300

    chore: add user model

---
+++ api/models/models.user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      lowerCase: true,
+      unique: true,
+      index: true,
+      match: [/\S+@\S+\.\S+/]
+    },
+    store: {
+      type: Schema.Types.ObjectId,
+      ref: 'store',
+      required: true
+    },
+    permission: {
+      type: Array,
+      default: ['client']
+    },
+    hash: String,
+    salt: String,
+    recovery: {
+      type: {
+        token: String,
+        date: Date
+      },
+      default: {}
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('user', userSchema)
