06c3670ef8098c165a49d9290cdf243a0ae65187
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-03 16:34:18 -0300

    chore: update database config

--- api/db/config.js
+++ api/db/config.js
@@ -1,10 +1,5 @@
-const nodeEnvironment = process.env.NODE_ENV || 'development'
-
-if (nodeEnvironment === 'development') {
-  require('dotenv').config()
-}
+require('dotenv').config()
 
 module.exports = {
-  uri: `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
-  nodeEnvironment
+  uri: `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`
 }
