bdc792034699c9553c3c86298904d502ad1231f4
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-09 15:58:51 -0300

    feat: ensure user email verification

---
+++ api/utils/middlewares/middlewares.verify-exists.js
@@ -0,0 +1,33 @@
+const { user } = require('../../models/models.index')
+const ErrorUnprocessableEntity = require('../errors/errors.unprocessable-entity')
+const ErrorBusinessRule = require('../errors/errors.business-rule')
+
+const verifyIdUserDbMiddleware = async (req, res, next) => {
+  const userDB = await user.findOne({ _id: req.params.userid })
+  if (!userDB) {
+    throw new ErrorUnprocessableEntity(`Não existe um usuário com esse id!`)
+  }
+  next()
+}
+
+const verifyEmailExists = async (req, res, next) => {
+  const resultEmail = await user.findOne({ email: req.body.email })
+  if (resultEmail) {
+    throw new ErrorBusinessRule('Este e-mail já está em uso!')
+  }
+  next()
+}
+
+const verifyCpfExists = async (req, res, next) => {
+  const resulCpf = await user.findOne({ email: req.body.cpf })
+  if (resulCpf !== null) {
+    throw new ErrorBusinessRule('Este cpf já está em uso!')
+  }
+  next()
+}
+
+module.exports = {
+  verifyIdUserDbMiddleware,
+  verifyEmailExists,
+  verifyCpfExists
+}
