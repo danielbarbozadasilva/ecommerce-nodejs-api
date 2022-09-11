2efede9a390248c5b02eb495bdd9c1cbe5aaf00a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-11 16:31:09 -0300

    refactor: ensure that the user can search solicitations client

--- api/mappers/mappers.client.js
+++ api/mappers/mappers.client.js
@@ -1,15 +1,18 @@
 const toDTO = (model) => ({
-  id: model._id,
-  cnpj: model.cnpj,
+  user: model.user,
   name: model.name,
-  email: model.email,
+  birthDate: model.birthDate,
+  cpf: model.cpf,
   phones: model.phones,
+  deleted: model.deleted,
+  store: model.store,
   address: {
     location: model.address.location,
     number: model.address.number,
     complement: model.address.complement,
     district: model.address.district,
     city: model.address.city,
+    state: model.address.state,
     zipCode: model.address.zipCode
   }
 })
