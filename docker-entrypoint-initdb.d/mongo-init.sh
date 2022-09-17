35d0c1109d98bb9e96f52cbe76c93e4d13ea0a86
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-17 12:25:35 -0300

    chore: add category seeders

--- docker-entrypoint-initdb.d/mongo-init.sh
+++ docker-entrypoint-initdb.d/mongo-init.sh
@@ -78,22 +78,25 @@ db.clients.insert([
         state: "Rio de janeiro"
     }
 }
+])
 
-db.createCollection("category")
-db.category.insert([
+db.createCollection("categories")
+db.categories.insert([
 {
     _id: ObjectId("6320f577156b47ff1082586e"),
     name: "Placa de vídeo",
     code: "43242342342343224",
     store: ObjectId("631e00f5190d800a90c6fa4d"),
-    availability: true
+    availability: true,
+    products: []
 },
 {
     _id: ObjectId("6320f577156b47ff1082586e"),
     name: "Processadores",
     code: "21341342342343551",
     store: ObjectId("631e00f5190d800a90c6fa4d"),
-    availability: false
+    availability: false,
+    products: []
 }
 ])
 
