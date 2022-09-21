28ca65a444e6afdf911727f5b1abaf9abd97c735
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-21 18:09:24 -0300

    refactor: user authorization

--- api/utils/middlewares/middlewares.authorization.js
+++ api/utils/middlewares/middlewares.authorization.js
@@ -4,12 +4,13 @@ const services = require('../../services/services.user')
 const authorizationMiddleware = (rule) => async (req, res, next) => {
   const { token } = req.headers
   const { id, permissions } = decodeToken(token)
-  const userid = req.params.clientid
+  const user = req.params.userid || req.params.clientid
 
   if (rule !== '*') {
     services.checkPermissionService(permissions, rule)
   }
-  await services.checkIdAuthorizationService(id, userid, permissions)
+
+  await services.checkIdAuthorizationService(id, user, permissions)
 
   next()
 }
