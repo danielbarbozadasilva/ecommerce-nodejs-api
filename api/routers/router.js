5346d5484b7d144f84492b3fbd619a4bb8093c0a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-22 13:00:10 -0300

    chore: add product route

--- api/routers/router.js
+++ api/routers/router.js
@@ -6,6 +6,7 @@ const routersV1User = require('./v1/routers.user')
 const routersV1Store = require('./v1/routers.store')
 const routersV1Client = require('./v1/routers.client')
 const routersV1Category = require('./v1/routers.category')
+const routersV1Product = require('./v1/routers.product')
 
 module.exports = (app) => {
   app.get('/', (req, res, next) => {
@@ -17,6 +18,7 @@ module.exports = (app) => {
   routersV1Store(routesV1)
   routersV1Client(routesV1)
   routersV1Category(routesV1)
+  routersV1Product(routesV1)
 
   app.use('/v1', routesV1)
 
