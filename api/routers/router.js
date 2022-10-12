7690fef86415b8575ed6f8e3614d4124550c7350
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-10-12 17:03:52 -0300

    refactor: remove store operations

--- api/routers/router.js
+++ api/routers/router.js
@@ -3,12 +3,11 @@ const { name, version } = require('../../package.json')
 const { formatError } = require('../utils/errors/errors.handler')
 
 const routersV1User = require('./v1/routers.user')
-const routersV1Store = require('./v1/routers.store')
 const routersV1Client = require('./v1/routers.client')
 const routersV1Category = require('./v1/routers.category')
 const routersV1Product = require('./v1/routers.product')
 const routersV1Rating = require('./v1/routers.rating')
-const routersV1Variations = require('./v1/routers.variations')
+const routersV1Solicitation = require('./v1/routers.solicitation')
 
 module.exports = (app) => {
   app.get('/', (req, res, next) => {
@@ -17,12 +16,11 @@ module.exports = (app) => {
 
   const routesV1 = Router()
   routersV1User(routesV1)
-  routersV1Store(routesV1)
   routersV1Client(routesV1)
   routersV1Category(routesV1)
   routersV1Product(routesV1)
   routersV1Rating(routesV1)
-  routersV1Variations(routesV1)
+  routersV1Solicitation(routesV1)
 
   app.use('/v1', routesV1)
 
