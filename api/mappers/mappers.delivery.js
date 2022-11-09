4347b256553610d921189e1831ccde538e5524d1
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-09 17:04:17 -0300

    fix: date format

--- api/mappers/mappers.delivery.js
+++ api/mappers/mappers.delivery.js
@@ -46,7 +46,9 @@ const toDTOList = (model) => ({
   type: model?.type,
   situation: model?.situation,
   payload: model?.payload,
-  date: model?.date.toLocaleTimeString('pt-BR')
+  date: `${new Date(model.date).toLocaleDateString('pt-BR')} - ${new Date(
+    model.date
+  ).toLocaleTimeString()}`
 })
 
 const toDTOShipping = (model) => ({
@@ -67,8 +69,98 @@ const toDTOShipping = (model) => ({
   comments: model.obsFim
 })
 
+const toDTOCart = (model) => {
+  let subTotal = 0
+  return {
+    id: model._id,
+    canceled: model.canceled,
+    cart: model.cart.map((item, i) => {
+      subTotal +=
+        (model.products[i].promotion || model.products[i].price) * item.quantity
+      return {
+        product: model.products[i],
+        quantity: item.quantity,
+        unitPrice: item.unitPrice.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+      }
+    }),
+    subTotal: subTotal.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    }),
+    shipping: model.shipping,
+    payment: {
+      id: model.payment._id,
+      price: model.payment.price.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+      }),
+      type: model.payment.type,
+      installments: model.payment.installments,
+      status: model.payment.status,
+      address: {
+        street: model.payment.address.street,
+        number: model.payment.address.number,
+        complement: model.payment.address.complement,
+        district: model.payment.address.district,
+        city: model.payment.address.city,
+        state: model.payment.address.state,
+        zipCode: model.payment.address.zipCode
+      },
+      addressDeliveryIgualCharging: model.payment.addressDeliveryIgualCharging,
+      pagSeguroCode: model.payment.pagSeguroCode
+    },
+    deliveries: {
+      id: model.deliveries._id,
+      status: model.deliveries.status,
+      trackingCode: model.deliveries.trackingCode,
+      type: model.deliveries.type,
+      price: model.deliveries.price.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+      }),
+      deliveryTime: model.deliveries.deliveryTime,
+      address: {
+        street: model.deliveries.address.street,
+        number: model.deliveries.address.number,
+        complement: model.deliveries.address.complement,
+        district: model.deliveries.address.district,
+        city: model.deliveries.address.city,
+        state: model.deliveries.address.state,
+        zipCode: model.deliveries.address.zipCode
+      }
+    },
+    client: {
+      id: model.client._id,
+      name: model.client.name,
+      birthDate: model.client.birthDate,
+      cpf: model.client.cpf,
+      phones: model.client.phones,
+      deleted: model.client.deleted,
+      address: {
+        street: model.payment.address.street,
+        number: model.payment.address.number,
+        complement: model.payment.address.complement,
+        district: model.payment.address.district,
+        city: model.payment.address.city,
+        state: model.payment.address.state,
+        zipCode: model.payment.address.zipCode
+      }
+    },
+    user: {
+      id: model.user._id,
+      name: model.user.name,
+      email: model.user.email,
+      permissions: model.user.permissions
+    }
+  }
+}
+
 module.exports = {
   toDTO,
   toDTOList,
-  toDTOShipping
+  toDTOShipping,
+  toDTOCart
 }
