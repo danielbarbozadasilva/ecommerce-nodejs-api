cb6ac42534d691f2d959262274172943e43d3e3b
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-03 15:25:43 -0300

    refactor: add rating average calculation

--- api/mappers/mappers.product.js
+++ api/mappers/mappers.product.js
@@ -3,33 +3,52 @@ const {
   formatAddressImage
 } = require('../utils/helpers/helpers.format')
 
-const toDTO = (model) => ({
-  id: model._id,
-  title: model.title,
-  availability: model.availability,
-  description: model.description,
-  photos: model.photos.map((item) => formatAddressImage(item)),
-  price: model.price,
-  promotion: model.promotion,
-  sku: model.sku,
-  quantity: model.quantity,
-  store: model.storeid,
-  dimensions: {
-    height: model.dimensions.height,
-    width: model.dimensions.width,
-    depth: model.dimensions.depth
-  },
-  weight: model.weight,
-  freeShipping: model.freeShipping,
-  category: {
-    _id: model.category._id,
-    name: model.category.name,
-    code: model.category.code,
-    store: model.category.store,
-    availability: model.category.availability,
-    products: model.category.products
+const toDTO = (model, metaData) => {
+  let media = 0
+  let cont = 0
+
+  return {
+    id: model._id,
+    title: model.title,
+    availability: model.availability,
+    description: model.description,
+    photos: model.photos.map((item) => formatAddressImage(item)),
+    price: model.price,
+    promotion: model.promotion,
+    sku: model.sku,
+    quantity: model.quantity,
+    store: model.storeid,
+    dimensions: {
+      height: model.dimensions.height,
+      width: model.dimensions.width,
+      depth: model.dimensions.depth
+    },
+    weight: model.weight,
+    freeShipping: model.freeShipping,
+    category: {
+      _id: model.category._id,
+      name: model.category.name,
+      code: model.category.code,
+      store: model.category.store,
+      availability: model.category.availability,
+      products: model.category.products
+    },
+    rating: model.rating.map((item) => {
+      media += item.score, 
+      cont++
+      return {
+        _id: item._id,
+        name: item.name,
+        text: item.text,
+        score: item.score,
+        product: item.product,
+        client: item.client
+      }
+    }),
+    total: media / cont,
+    metaData: metaData.total
   }
-})
+}
 
 const toDTORating = (model) => ({
   id: model._id,
