0f1775a781d2d3453145ade755035c60afb4677f
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-12 18:25:41 -0300

    test: ensure listCategoryByIdService return success

--- __tests__/unit/services/category-services.spec.js
+++ __tests__/unit/services/category-services.spec.js
@@ -15,9 +15,18 @@ describe('Category services', () => {
       const result = await services.listAllCategoryService()
       expect(result.success).toBe(true)
     })
+
     test('Make sure listAllCategoryService has the id property', async () => {
       const result = await services.listAllCategoryService()
       expect(result.data[0].id).toHaveProperty('id')
     })
+
+    test('Make sure listCategoryByIdService return success', async () => {
+      const id = '6320f577156b47ff1082586e'
+      const result = await services.listCategoryByIdService(id)
+      expect(result.success).toBe(true)
+    })
+
+   
   })
 })
