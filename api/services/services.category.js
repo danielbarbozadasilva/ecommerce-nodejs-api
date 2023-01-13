9953e4ffe2500afe867ae5e53cc8feea6a7af138
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-13 15:58:09 -0300

    refactor: clean and optimize the code

--- api/services/services.category.js
+++ api/services/services.category.js
@@ -44,7 +44,7 @@ const createCategoryService = async (body, files) => {
       name: body.name,
       code: body.code,
       availability: true,
-      photo: files[0].filename
+      photo: files.length ? files[0].filename : ''
     })
 
     return {
@@ -61,13 +61,10 @@ const updateCategoryService = async (categoryid, body, files) => {
   try {
     const result = await category.findOne({ _id: categoryid })
 
-    if (files.length) {
-      result.photo = files[0].filename
-    }
-
     result.name = body.name
     result.code = body.code
     result.availability = body.availability
+    result.photo = files.length ? files[0].filename : ''
 
     result.save()
 
