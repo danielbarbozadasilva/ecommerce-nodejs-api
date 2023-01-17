cfa8cba460a45db1405e4d58dcc0e27575336459
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-17 13:11:44 -0300

    test: ensure updateQuantityConfirm return error

--- __tests__/unit/services/payment-services.spec.js
+++ __tests__/unit/services/payment-services.spec.js
@@ -40,5 +40,75 @@ describe('Payment services', () => {
         ).toThrow()
       } catch (error) {}
     })
+
+    test('Make sure sendEmailAdmSuccessfullyPaid return success', async () => {
+      try {
+        const solicitationNumber = '4777720845'
+        expect(
+          await services.sendEmailAdmSuccessfullyPaid(solicitationNumber)
+        ).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailAdmSuccessfullyPaid return error', async () => {
+      try {
+        const solicitationNumber = '2737119823'
+        expect(
+          await services.sendEmailAdmSuccessfullyPaid(solicitationNumber)
+        ).toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailClientPaymentFailed return success', async () => {
+      try {
+        const solicitationNumber = '4777720845'
+        expect(
+          await services.sendEmailClientPaymentFailed(solicitationNumber)
+        ).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailClientPaymentFailed return error', async () => {
+      try {
+        const solicitationNumber = '2737119823'
+        expect(
+          await services.sendEmailClientPaymentFailed(solicitationNumber)
+        ).toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailAdmPaymentFailed return success', async () => {
+      try {
+        const solicitationNumber = '4777720845'
+        expect(
+          await services.sendEmailAdmPaymentFailed(solicitationNumber)
+        ).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailAdmPaymentFailed return error', async () => {
+      try {
+        const solicitationNumber = '2737119823'
+        expect(
+          await services.sendEmailAdmPaymentFailed(solicitationNumber)
+        ).toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure updateQuantityConfirm return success', async () => {
+      try {
+        const solicitationId = '63c59bb0107f4ce9de7fd63d'
+        expect(
+          await services.updateQuantityConfirm(solicitationId)
+        ).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure updateQuantityConfirm return error', async () => {
+      try {
+        const solicitationId = '61c59bb0107f4ce9de7fd32c'
+        expect(await services.updateQuantityConfirm(solicitationId)).toThrow()
+      } catch (error) {}
+    })
   })
 })
