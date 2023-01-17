c331c3b7ff844858689069761ef0a355bccbb80e
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-17 06:54:16 -0300

    test: ensure sendEmailClientSuccessPaid return error

--- __tests__/unit/services/payment-services.spec.js
+++ __tests__/unit/services/payment-services.spec.js
@@ -16,10 +16,20 @@ describe('Payment services', () => {
       const result = await services.listByIdPaymentService(paymentId)
       expect(result.success).toBe(true)
     })
+
     test('Make sure listByIdPaymentService has the id property', async () => {
       const paymentId = '63c59bb0107f4ce9de7fd638'
       const result = await services.listByIdPaymentService(paymentId)
       expect(result.data.payment).toHaveProperty('id')
+    })
+
+    test('Make sure sendEmailClientSuccessPaid return error', async () => {
+      try {
+        const solicitationNumber = '21223e4e412312313'
+        expect(
+          await services.sendEmailClientSuccessPaid(solicitationNumber)
+        ).toThrow()
+      } catch (error) {}
     })
   })
 })
