8f5ba9974f13cf7d882fed33cb8e796d874509bd
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 11:32:55 -0300

    test: ensure updateQuantityCancelation return success

--- __tests__/unit/services/solicitation-service.spec.js
+++ __tests__/unit/services/solicitation-service.spec.js
@@ -1,5 +1,9 @@
 const services = require('../../../api/services/services.solicitation')
 const { createConnection, closeConnection } = require('../../helpers')
+const {
+  mockSolicitationError,
+  mockSolicitationSuccess
+} = require('../../mocks')
 
 describe('Solicitation services', () => {
   beforeAll(() => {
@@ -31,6 +35,30 @@ describe('Solicitation services', () => {
         const solicitationNumber = '234120127'
         expect(
           await services.listByNumberSolicitationService(solicitationNumber)
+        ).toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure listByNumberSolicitationService return success', async () => {
+      const solicitationNumber = '4777720845'
+      const result = await services.listByNumberSolicitationService(
+        solicitationNumber
+      )
+      expect(result.success).toBe(true)
+    })
+
+    test('Make sure updateQuantityCancelation return success', async () => {
+      try {
+        expect(
+          await services.updateQuantityCancelation(mockSolicitationSuccess)
+        ).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure updateQuantityCancelation return error', async () => {
+      try {
+        expect(
+          await services.updateQuantityCancelation(mockSolicitationError)
         ).toThrow()
       } catch (error) {}
     })
