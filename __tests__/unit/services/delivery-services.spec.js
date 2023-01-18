06101d3f1b8ae0926eebf6dff25cfe7fe2b9d068
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 06:33:08 -0300

    test: ensure searchCartSolicitation return success

--- __tests__/unit/services/delivery-services.spec.js
+++ __tests__/unit/services/delivery-services.spec.js
@@ -16,5 +16,19 @@ describe('Delivery services', () => {
       const result = await services.listByIdDeliveryService(deliveryId)
       expect(result.success).toBe(true)
     })
+
+    test('Make sure listByIdDeliveryService has the id property', async () => {
+      const deliveryId = '63c59bb0107f4ce9de7fd63a'
+      const result = await services.listByIdDeliveryService(deliveryId)
+      expect(result.data.id).toHaveProperty('id')
+    })
+
+    test('Make sure searchCartSolicitation return success', async () => {
+      const solicitationId = '63c59bb0107f4ce9de7fd63d'
+      const result = await services.searchCartSolicitation(solicitationId)
+      expect(result.success).toBe(true)
+    })
+
+   
   })
 })
