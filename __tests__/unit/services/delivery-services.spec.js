c85632b5f0361d96fe328c1624b10a82ef2055ab
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 07:05:21 -0300

    test: ensure calculateShippingService has the price property

--- __tests__/unit/services/delivery-services.spec.js
+++ __tests__/unit/services/delivery-services.spec.js
@@ -1,5 +1,6 @@
 const services = require('../../../api/services/services.delivery')
 const { createConnection, closeConnection } = require('../../helpers')
+const { mockDelivery, mockDeliveryShipping } = require('../../mocks')
 
 describe('Delivery services', () => {
   beforeAll(() => {
@@ -47,6 +48,38 @@ describe('Delivery services', () => {
         const solicitationId = '21c49bb0107f4ce9de7fd61b'
         expect(await services.sendEmailUpdate(solicitationId)).toThrow()
       } catch (error) {}
+    })
+
+    test('Make sure updateDeliveryService return success', async () => {
+      const deliveryId = '63c59bb0107f4ce9de7fd63a'
+      const result = await services.updateDeliveryService(
+        mockDelivery,
+        deliveryId
+      )
+      expect(result.success).toBe(true)
+    })
+
+    test('Make sure updateDeliveryService has the id property', async () => {
+      const deliveryId = '63c59bb0107f4ce9de7fd63a'
+      const result = await services.updateDeliveryService(
+        mockDelivery,
+        deliveryId
+      )
+      expect(result.data).toHaveProperty('date')
+    })
+
+    test('Make sure calculateShippingService return success', async () => {
+      const result = await services.calculateShippingService(
+        mockDeliveryShipping
+      )
+      expect(result.success).toBe(true)
+    })
+
+    test('Make sure calculateShippingService has the price property', async () => {
+      const result = await services.calculateShippingService(
+        mockDeliveryShipping
+      )
+      expect(result.data[0]).toHaveProperty('price')
     })
   })
 })
