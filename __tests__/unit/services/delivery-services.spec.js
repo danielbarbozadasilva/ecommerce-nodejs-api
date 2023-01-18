dc9b870f9c03e61854299d143ab79e87bac8abfa
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 06:38:13 -0300

    test: ensure sendEmailUpdate return success

--- __tests__/unit/services/delivery-services.spec.js
+++ __tests__/unit/services/delivery-services.spec.js
@@ -29,6 +29,24 @@ describe('Delivery services', () => {
       expect(result.success).toBe(true)
     })
 
-   
+    test('Make sure searchCartSolicitation has the id property', async () => {
+      const solicitationId = '63c59bb0107f4ce9de7fd63d'
+      const result = await services.searchCartSolicitation(solicitationId)
+      expect(result.data.id).toHaveProperty('id')
+    })
+
+    test('Make sure sendEmailUpdate return success', async () => {
+      try {
+        const solicitationId = '63c59bb0107f4ce9de7fd63d'
+        expect(await services.sendEmailUpdate(solicitationId)).not.toThrow()
+      } catch (error) {}
+    })
+
+    test('Make sure sendEmailUpdate return error', async () => {
+      try {
+        const solicitationId = '21c49bb0107f4ce9de7fd61b'
+        expect(await services.sendEmailUpdate(solicitationId)).toThrow()
+      } catch (error) {}
+    })
   })
 })
