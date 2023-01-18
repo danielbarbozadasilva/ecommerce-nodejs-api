e1375aa60c10290a14d213c859c04d7093d9011e
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 06:03:11 -0300

    test: ensure listRatingProductService return success

--- __tests__/unit/services/rating-sevices.spec.js
+++ __tests__/unit/services/rating-sevices.spec.js
@@ -15,9 +15,16 @@ describe('Rating services', () => {
       const result = await services.listAllRatingService()
       expect(result.success).toBe(true)
     })
+
     test('Make sure listAllRatingService has the id property', async () => {
       const result = await services.listAllRatingService()
       expect(result.data[0].id).toHaveProperty('id')
+    })
+
+    test('Make sure listRatingProductService return success', async () => {
+      const productId = '63432f02a7f855351c99dc71'
+      const result = await services.listRatingProductService(productId)
+      expect(result.success).toBe(true)
     })
   })
 })
