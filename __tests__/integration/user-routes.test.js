7b90915a842e6db861a0318df9e8b552371a2d25
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-18 14:06:28 -0300

    test: ensure /v1/auth return 401 if the credentials are not valid

--- __tests__/integration/user-routes.test.js
+++ __tests__/integration/user-routes.test.js
@@ -22,5 +22,14 @@ describe('Auth Routes', () => {
         .expect(200)
     })
 
+    test('Make sure /v1/auth return 401 if the credentials are not valid', async () => {
+      await request(app)
+        .post('/v1/auth')
+        .send({
+          email: 'exemplo01@gmail.com',
+          password: 'exemplo01'
+        })
+        .expect(401)
+    })
   })
 })
