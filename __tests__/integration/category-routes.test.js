7d6c0d89803b05367efcead9ebe606355ab348bc
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-21 05:28:59 -0300

    test: ensure /v1/category/:categoryid return 200 on category delete

--- __tests__/integration/category-routes.test.js
+++ __tests__/integration/category-routes.test.js
@@ -17,6 +17,7 @@ describe('Category Routes', () => {
       await request(app).get(`/v1/category`).expect(200)
     })
   })
+
   describe('Route POST /v1/category', () => {
     test('Make sure /v1/category return 200 on create category', async () => {
       const email = 'danielbarboza56@hotmail.com'
@@ -42,6 +43,7 @@ describe('Category Routes', () => {
         .expect(401)
     })
   })
+
   describe('Route GET /v1/category/:categoryid', () => {
     test('Make sure /v1/category/:categoryid return 200 on category id search', async () => {
       const categoryid = '63433ba03a63723e66f5f62d'
@@ -51,6 +53,72 @@ describe('Category Routes', () => {
     test('Make sure /v1/category/:categoryid return 422 if category id does not exist', async () => {
       const categoryid = '63433ba03a63723e66f5f61c'
       await request(app).get(`/v1/category/${categoryid}`).expect(422)
+    })
+  })
+
+  describe('Route PUT /v1/category/:categoryid', () => {
+    test('Make sure /v1/category/:categoryid return 200 on category id update', async () => {
+      const categoryid = '63433ba03a63723e66f5f62d'
+      const email = 'danielbarboza56@hotmail.com'
+      const result = await createCredentialService(email)
+      await request(app)
+        .put(`/v1/category/${categoryid}`)
+        .send({
+          name: 'Cameras',
+          code: '51421148302633523',
+          photo: ''
+        })
+        .set(result)
+        .expect(200)
+    })
+
+    test('Make sure /v1/category/:categoryid return 401 on category id update', async () => {
+      const categoryid = '63433ba03a63723e66f5f62d'
+      await request(app)
+        .put(`/v1/category/${categoryid}`)
+        .send({
+          name: 'Cameras',
+          code: '51421148302633523',
+          photo: ''
+        })
+        .expect(401)
+    })
+
+    test('Make sure /v1/category/:categoryid return 422 if category id does not exist', async () => {
+      const categoryid = '63433ba03a63723e66f5f61f'
+      const email = 'danielbarboza56@hotmail.com'
+      const result = await createCredentialService(email)
+      await request(app)
+        .put(`/v1/category/${categoryid}`)
+        .send({
+          name: 'Cameras',
+          code: '51421148302633523',
+          photo: ''
+        })
+        .set(result)
+        .expect(422)
+    })
+  })
+
+  describe('Route DELETE /v1/category/:categoryid', () => {
+    test('Make sure /v1/category/:categoryid return 200 on category delete', async () => {
+      const categoryid = '6320f577156b47ff1082586e'
+      const email = 'danielbarboza56@hotmail.com'
+      const result = await createCredentialService(email)
+      await request(app)
+        .delete(`/v1/category/${categoryid}`)
+        .set(result)
+        .expect(200)
+    })
+
+    test('Make sure /v1/category/:categoryid return 422 if category id does not exist', async () => {
+      const categoryid = '63433ba03a63723e66f5f61f'
+      const email = 'danielbarboza56@hotmail.com'
+      const result = await createCredentialService(email)
+      await request(app)
+        .delete(`/v1/category/${categoryid}`)
+        .set(result)
+        .expect(422)
     })
   })
 })
