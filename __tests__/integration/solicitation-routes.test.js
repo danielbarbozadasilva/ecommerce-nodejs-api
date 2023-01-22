ea14d60fbf06f2aaf716bd21c7d0cb15760906cb
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-22 03:22:37 -0300

    test: ensure /v1/solicitation return 200 on create solicitation

--- __tests__/integration/solicitation-routes.test.js
+++ __tests__/integration/solicitation-routes.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const app = require('../../api/app')
 const { createCredentialService } = require('../../api/services/services.user')
 const { createConnection, closeConnection } = require('../helpers')
+const { mockSolicitationSuccess } = require('../mocks')
 
 describe('Solicitation Routes', () => {
   beforeAll(() => {
@@ -17,6 +18,18 @@ describe('Solicitation Routes', () => {
       const email = 'danielbarboza56@hotmail.com'
       const result = await createCredentialService(email)
       await request(app).get(`/v1/solicitation`).set(result).expect(200)
+    })
+  })
+  describe('Route POST /v1/solicitation', () => {
+    test('Make sure /v1/solicitation return 200 on create solicitation', async () => {
+      const email = 'daniel95barboza@gmail.com'
+      const clientid = '6320f577156b47ff1082586e'
+      const result = await createCredentialService(email)
+      await request(app)
+        .post(`/v1/solicitation?clientid=${clientid}`)
+        .send(mockSolicitationSuccess)
+        .set(result)
+        .expect(200)
     })
   })
 })
